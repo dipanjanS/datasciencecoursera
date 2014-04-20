c59d42df9928dd0c08a040b5e2c5d3feb508bd48
Dipanjan Sarkar <dipanzan.sarkar@gmail.com>
2014-04-20 20:13:03 +0530

    added code to cacheSolve

--- RProgramming/ProgrammingAssignment2/cachematrix.R
+++ RProgramming/ProgrammingAssignment2/cachematrix.R
@@ -33,4 +33,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinv()
+  
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+  
 }
